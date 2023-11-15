6f7bc49cf952d8b98914be55cac83b8f2b40bac8
yuna-c <yuna744@gmail.com>
2023-11-15 12:01:40 +0900

    1115 현재 시간이 13이 넘어가면 01시로 출력

--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
 const screen = document.querySelector('.screen');
 const em = screen.querySelector('em');
-const numbers = screen.querySelectorAll('span');
+const numbers = screen.querySelectorAll('span'); // 배열
 
 setInterval(() => {
-	/*
-	spanHr.innerText = hr < 10 ? '0' + hr : hr;
-	spanMin.innerText = min < 10 ? '0' + min : min;
-	spanSec.innerText = sec < 10 ? '0' + sec : sec;
-  */
-
-	em.innerText = new Date().getHours() /*hr*/ < 12 ? 'am' : 'pm';
+	em.innerText = new Date().getHours() < 12 ? 'am' : 'pm';
 	//getTime함수가 [시간,분,초]반환
 	//반환된 배열값을 그대로 반복돌면서 setTime함수에 인수로 전달
 	//setTime반복돌면서 시간,분,초에 1자리수일때 앞에 '0'을 붙여주는 공통로직 반복처리
+
 	getTime().forEach((num, idx) => setTime(num, idx));
 }, 1000);
 
@@ -23,10 +18,18 @@ function getTime() {
 	let hr = now.getHours();
 	let min = now.getMinutes();
 	let sec = now.getSeconds();
+
+	// 현재 시간값이 13이 넘어가면 13시가 아닌 01시로 출력
+	if (hr > 12) {
+		hr = hr - 12;
+	} else {
+		hr;
+	}
+	// hr = hr > 12 ? hr-12 : hr
 	return [hr, min, sec];
 }
 
 //반환된 시간값을 인수로 받아서 DOM에 세팅하는 함수
-function setTime(num, index) {
-	numbers[index].innerText = num < 10 ? '0' + num : num;
+function setTime(num, idx) {
+	numbers[idx].innerText = num < 10 ? '0' + num : num;
 }
