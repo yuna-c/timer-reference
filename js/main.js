ea76280f75b4b5dd02e309c263d1ac8a6024c74a
yuna-c <yuna744@gmail.com>
2023-11-15 11:31:05 +0900

    1115 전자시계 출력

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 const screen = document.querySelector('.screen');
 const [spanHr, spanMin, spanSec] = screen.querySelectorAll('span');
-const now = new Date();
-let hr = now.getHours() < 10 ? '0' + now.getHours() : now.getHours();
-let min = now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes();
-let sec = now.getSeconds() < 10 ? '0' + now.getSeconds() : now.getSeconds();
 
-spanHr.innerText = hr;
-spanMin.innerText = min;
-spanSec.innerText = sec;
+setInterval(() => {
+	const now = new Date();
+	let hr = now.getHours();
+	let min = now.getMinutes();
+	let sec = now.getSeconds();
+
+	spanHr.innerText = hr < 10 ? '0' + hr : hr;
+	spanMin.innerText = min < 10 ? '0' + min : min;
+	spanSec.innerText = sec < 10 ? '0' + sec : sec;
+}, 1000);
+setInterval();
