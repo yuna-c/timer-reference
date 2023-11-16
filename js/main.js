eb6c614c2002224b85c0544cb14f5214c681924e
yuna-c <yuna744@gmail.com>
2023-11-16 10:16:56 +0900

    1116 setInterval 호출형태, 정의형태

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ const data = [
 //1초마다 전자시계 출력 함수 호출
 //특정함수에 콜백함수를 전달할 때 함수 호출 구문이 아닌 정의문 형태로 전달
 //setWatch처럼 함수명만 넣으면 정의형태이기 때문에 바로등록하능
-setInterval(setWatch, 1000);
+setInterval(setWatch, 1000); //호출문
 
 //changeTheme의 경우는 data라는 인수를 전달해야 하기 때문에 () 붙여야 함
 //()를 붙이는 순간에 정의형태가 아닌 호출형태로 변경되므로 다시 익명함수로 호출문을 Wrapping해서 정의형태로 변경
@@ -22,7 +22,7 @@ setInterval(setWatch, 1000);
 // 	changeTheme(data);
 // 	setWatch();
 // }, 1000);
-let timer = setInterval(() => changeTheme(data), 1000);
+let timer = setInterval(() => changeTheme(data), 1000); //()=>{blabla(),1000} 익명함수로 감싸서 정의 형태로 변경(외부 함수 자체를 불러오기 때문에, 원하는 시점에 호출 시켜야 되니까 묶어놔야지!)
 
 btns.forEach((btn) => {
 	btn.addEventListener('click', (e) => {
