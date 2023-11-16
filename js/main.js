97ffe5279666c8ce62dbd5cc3b73721ebe938c18
yuna-c <yuna744@gmail.com>
2023-11-16 10:11:24 +0900

    1116 콜백함수로 인수 전달시 함수명만 쓰는 경우와, 익명함수로 감싸는 차이

--- js/main.js
+++ js/main.js
@@ -1,85 +1,64 @@
 const main = document.querySelector('main');
 const screen = document.querySelector('.screen');
 const em = screen.querySelector('em');
-const numbers = screen.querySelectorAll('span'); // 배열
-const txt = screen.querySelectorAll('em'); // em 두개 해서 am/pm 오전 오후 바뀌게 해보기
+const numbers = screen.querySelectorAll('span');
 const btns = main.querySelectorAll('nav span');
-console.log(btns);
 
-// 자주 바뀔만한 값을 전역변수 형태로 객체를 배열로 묶어두는 형태로 따로 빼서 관리
-// 해당 값이 아래 함수에서 호출되도록 처리
-// const hr = new Date().getHours(); 이거 왜 안대?
 const data = [
-	{ cond: new Date().getHours() >= 5 && new Date().getHours() < 10, name: 'morning' },
-	{ cond: new Date().getHours() >= 12 && new Date().getHours() < 16, name: 'afeternoon' },
+	{ cond: new Date().getHours() >= 5 && new Date().getHours() < 11, name: 'morning' },
+	{ cond: new Date().getHours() >= 11 && new Date().getHours() < 16, name: 'afeternoon' },
 	{ cond: new Date().getHours() >= 16 && new Date().getHours() < 20, name: 'evening' },
 	{ cond: new Date().getHours() >= 20 || new Date().getHours() < 5, name: 'night' },
 ];
 
-let timer = setInterval(() => {
-	//data전역변수를 인수로 받아서 호출처리 (파라미터화)
-	changeTheme(data);
-	em.innerText = new Date().getHours() < 12 ? 'am' : 'pm';
-	getTime().forEach((num, idx) => setTime(num, idx));
-}, 1000);
+//1초마다 전자시계 출력 함수 호출
+//특정함수에 콜백함수를 전달할 때 함수 호출 구문이 아닌 정의문 형태로 전달
+//setWatch처럼 함수명만 넣으면 정의형태이기 때문에 바로등록하능
+setInterval(setWatch, 1000);
+
+//changeTheme의 경우는 data라는 인수를 전달해야 하기 때문에 () 붙여야 함
+//()를 붙이는 순간에 정의형태가 아닌 호출형태로 변경되므로 다시 익명함수로 호출문을 Wrapping해서 정의형태로 변경
+// let timer = setInterval(() => {
+// 	changeTheme(data);
+// 	setWatch();
+// }, 1000);
+let timer = setInterval(() => changeTheme(data), 1000);
 
 btns.forEach((btn) => {
-	//각 버튼 클릭시
 	btn.addEventListener('click', (e) => {
-		//클릭한 버튼만 활성화
 		btns.forEach((btn) => btn.classList.remove('on'));
 		e.currentTarget.classList.add('on');
-		// 기존 자동롤링기능 끊어줌
+
 		clearInterval(timer);
-		//메인 요소의 모든 클래스 제거
 		main.className = '';
-		// 클릭한 버튼의 글자를 가져와서 소문자로 변경한 다음 메인 요소의 클래스명으로 지정
 		main.classList.add(e.currentTarget.innerText.toLowerCase());
 	});
 });
 
-//시간값을 구해서 반환하는 함수
+//?
+function setWatch() {
+	em.innerText = new Date().getHours() < 12 ? 'am' : 'pm';
+	getTime().forEach((num, idx) => setTime(num, idx));
+}
+
 function getTime() {
-	//지역변수로 떼내기
 	const now = new Date();
 	let hr = now.getHours();
 	let min = now.getMinutes();
 	let sec = now.getSeconds();
 
-	// 현재 시간값이 13이 넘어가면 13시가 아닌 01시로 출력
-	if (hr > 12) {
-		hr = hr - 12;
-	} else {
-		hr;
-	}
-	// hr = hr > 12 ? hr-12 : hr
+	hr = hr > 12 ? hr - 12 : hr;
 	return [hr, min, sec];
 }
 
-//반환된 시간값을 인수로 받아서 DOM에 세팅하는 함수
 function setTime(num, idx) {
 	numbers[idx].innerText = num < 10 ? '0' + num : num;
-	// numbers도 파라미터로 받는게 맞음 (잘못 들어갔을 경우)
-	// 호출 될 때//
 }
 
-//시간에 따른 테마 변경함수
 function changeTheme(data) {
-	// const hr = new Date().getHours();
-	//전역 data를 바로 활용하는 것이 아닌 info라는 파라미터를 통해서 전달받도록 처리
 	main.className = '';
 
 	data.forEach((el) => {
 		if (el.cond) main.classList.add(el.name);
 	});
-	/*
-  const changeData = [
-    {time : hr >= 5 && hr < 10, value : 'morning'},
-    {time : hr >= 12 && hr < 16, value : 'afternoon'},
-    {time : hr >= 16 && hr < 20, value : 'evening'},
-    {time : hr >= 20 || hr < 5, value : 'night'}
-  ]
-  */
 }
-
-//clearinterver
